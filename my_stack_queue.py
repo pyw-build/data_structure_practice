31216efafa65bf0ecb12a91347625dce1c2fd0cf
YA <poyawu419@gmail.com>
2020-03-29 14:37:24 +0800

    Add Queue[C

--- my_stack_queue.py
+++ my_stack_queue.py
@@ -1,4 +1,4 @@
-class myStackNode:
+class myNode:
     def __init__(self, content_in):
         self.content = content_in
 
@@ -6,7 +6,6 @@ class myStack:
     def __init__(self, size):
          self.maxSize = size
          self.stackList = []
-
     def push(self,new_node):
         if len(self.stackList)==self.maxSize:
             print("Stack full! Cannot push!")
@@ -25,14 +24,51 @@ class myStack:
     def isEmpty(self):
         return len(self.stackList) == 0
 
+class myQueue:
+    def __init__(self, size):
+        self.maxSize = size
+        self.queueList = []  #front: , rear: 
+    def add(self,new_node):
+        if len(self.queueList)==self.maxSize:
+            print("Queue full! Cannot add!")
+        else:
+            self.queueList.append(new_node)
+    def delete(self):
+        if len(self.queueList)==0:
+            print("Queue empty! Cannot delete!")
+        else:
+            delItem = self.queueList[0]
+            del self.queueList[0]
+            return delItem
+    def front(self):
+        if len(self.queueList)!=0:
+            return self.queueList[0]
+    def rear(self):
+        if len(self.queueList)!=0:
+            return self.queueList[-1]
+    def isFull(self):
+        return len(self.queueList) == self.maxSize
+    def isEmpty(self):
+        return len(self.queueList) == 0
+
 if __name__ == "__main__":
     myStacktest = myStack(5)
     #Push test
     for i in range(6):
-        myStacktest.push(myStackNode(i*101))
+        myStacktest.push(myNode(i*101))
         #print("Push: "+str(i*101))
     #Pop test
     for i in range(6):
         popItem = myStacktest.pop()
         if popItem!=None:
             print("Pop: "+str(popItem.content))
+
+    myQueuetest = myQueue(5)
+    #Add test
+    for i in range(6):
+        myQueuetest.add(myNode(i*11))
+    #Delete test
+    for i in range(6):
+        delItem = myQueuetest.delete()
+        if delItem!=None:
+            print("Delete: "+str(delItem.content))
